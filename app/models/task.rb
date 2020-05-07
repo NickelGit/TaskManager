bc1618b04685d49df10ce346d6365758b75f3956
NickelGit <old_nickel@mail.ru>
2020-05-07 13:45:42 +0400

    States definitions added

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,30 +8,37 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
-  state_machine :initial => :new_task do
+  state_machine initial: :new_task do
+    state :new_task
     state :in_development do
       validates :assignee, presence: true
     end
-    event :assign do      
-      transition :new_task => :in_development
+    state :in_qa
+    state :in_code_review
+    state :ready_for_release
+    state :released
+    state :archived
+
+    event :assign do
+      transition new_task: :in_development
     end
     event :develop do
-      transition :in_development => :in_qa
+      transition in_development: :in_qa
     end
     event :return_to_develop do
-      transition [:in_qa, :in_code_review] => :in_development
+      transition %i[in_qa in_code_review] => :in_development
     end
     event :qa_pass do
-      transition :in_qa => :in_code_review
+      transition in_qa: :in_code_review
     end
     event :code_review_pass do
-      transition :in_code_review => :ready_for_release
+      transition in_code_review: :ready_for_release
     end
     event :release do
-      transition :ready_for_release => :released
+      transition ready_for_release: :released
     end
     event :archive do
-      transition [:new_task, :released] => :archived
+      transition %i[new_task released] => :archived
     end
   end
 end
