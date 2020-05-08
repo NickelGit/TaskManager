72adff110c17d81f34dffc8e9e8b7a677c426878
NickelGit <old_nickel@mail.ru>
2020-05-08 16:23:50 +0400

    Task factory fixed. Task factory test added

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -4,9 +4,11 @@ FactoryBot.define do
   factory :task do
     name { generate :task_name }
     description { generate :task_description }
-    author_id {}
-    assignee_id {}
+    author factory: :manager
     state { 'new_task' }
     expired_at { DateTime.now + 15 }
+    trait :assigned do
+      assignee factory: :developer
+    end
   end
 end
