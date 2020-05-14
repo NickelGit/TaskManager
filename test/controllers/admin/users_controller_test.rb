564c7ed5b02bbd75d57a416181ae160aedb99e16
NickelGit <old_nickel@mail.ru>
2020-05-14 17:31:49 +0400

    Users_controller tests improved

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -29,20 +29,43 @@ class Admin::UsersControllerTest < ActionController::TestCase
 
   test 'should post create' do
     user = attributes_for(:user)
+    assert_not User.find_by(email: user[:email]).present?
+
     post :create, params: { user: user }
-    assert_response :redirect
+    created_user = User.find_by(email: user[:email])
+
+    assert created_user.present?
+    assert created_user[:first_name] == user[:first_name]
+    assert created_user[:last_name] == user[:last_name]
+    assert created_user.authenticate(user[:password])
+    assert created_user[:type] == user[:type]
   end
 
   test 'should patch update' do
     user = create(:user)
-    user_attrs = attributes_for(:user)
+    user_attrs = attributes_for(:developer)
+
     patch :update, params: { id: user.id, user: user_attrs }
-    assert_response :redirect
+    patched_user = User.find(user.id)
+
+    assert patched_user[:first_name] == user_attrs[:first_name]
+    assert patched_user[:last_name] == user_attrs[:last_name]
+    assert patched_user.authenticate(user_attrs[:password])
+    assert patched_user[:type] == user_attrs[:type]
+
+    new_user_attrs = attributes_for(:manager)
+    patch :update, params: { id: user.id, user: new_user_attrs }
+    patched_user = User.find(user.id)
+
+    assert patched_user[:first_name] == new_user_attrs[:first_name]
+    assert patched_user[:last_name] == new_user_attrs[:last_name]
+    assert patched_user.authenticate(new_user_attrs[:password])
+    assert patched_user[:type] == new_user_attrs[:type]
   end
 
   test 'should delete destroy' do
     user = create(:user)
     delete :destroy, params: { id: user.id }
-    assert_response :redirect
+    assert_not User.find_by(id: user.id).present?
   end
 end
