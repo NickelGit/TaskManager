27e8f5de997f0bf711f47fecf57f6210fba8c525
NickelGit <old_nickel@mail.ru>
2020-05-18 14:54:02 +0400

    Task API controllers and tests added

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::TasksController < Api::V1::ApplicationController
+  def show; end
+
+  def index; end
+
+  def new; end
+
+  def create; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def task_params; end
+end
