1384f2c4ed4357778a9ab96d4b323ab0b7a1831b
NickelGit <old_nickel@mail.ru>
2020-05-18 16:17:40 +0400

    #index method added

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::TasksController < Api::V1::ApplicationController
+  respond_to :json
+
   def show; end
 
-  def index; end
+  def index
+    tasks = Task.all.
+      ransack(ransack_params).
+      result.
+      page(page).
+      per(per_page)
+    respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+  end
 
   def new; end
 
