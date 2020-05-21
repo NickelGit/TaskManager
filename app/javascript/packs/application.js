1f58b8fa6eebabe161b24cf041977befd98a737d
NickelGit <old_nickel@mail.ru>
2020-05-21 17:56:49 +0400

    Script for check added to package.json. Linter errors autofixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
-import 'material-design-lite/material.js'
-import WebpackerReact from 'webpacker-react'
-import TaskBoard from './components/TaskBoard'
+require('@rails/ujs').start();
+require('@rails/activestorage').start();
+require('channels');
 
-WebpackerReact.setup({ TaskBoard })
+import 'material-design-lite/material.js';
+import WebpackerReact from 'webpacker-react';
+import TaskBoard from './components/TaskBoard';
+
+WebpackerReact.setup({ TaskBoard });
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
