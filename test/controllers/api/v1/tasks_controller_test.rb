a014f974f41fc5e195f70960d01d066327a8fa30
NickelGit <old_nickel@mail.ru>
2020-06-17 18:09:55 +0400

    Notifications added to update and delete processes

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -40,7 +40,9 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
       merge({ author_id: author.id, assignee_id: assignee.id }).
       stringify_keys
 
-    patch :update, params: { id: task.id, format: :json, task: task_attributes }
+    assert_emails 1 do
+      patch :update, params: { id: task.id, format: :json, task: task_attributes }
+    end
     assert_response :success
 
     task.reload
@@ -50,7 +52,9 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
   test 'should delete destroy' do
     author = create(:user)
     task = create(:task, author: author)
-    delete :destroy, params: { id: task.id, format: :json }
+    assert_emails 1 do
+      delete :destroy, params: { id: task.id, format: :json }
+    end
     assert_response :success
 
     assert !Task.where(id: task.id).exists?
